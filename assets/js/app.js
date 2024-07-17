23932d81513a2efc747e936b08e1bd0cb714cf73
YamrajShrestha <144941654+YamrajShrestha@users.noreply.github.com>
2024-07-17 14:42:55 +0545

    navbar responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,22 @@ scrollUp.addEventListener("click", () => {
         behavior: "smooth",
     });
 });
+
+// Nav hamburgerburger selections
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+
+burger.addEventListener("click", () => {
+    ul.classList.toggle("show");
+});
+
+// Close hamburger menu when a link is clicked
+// Select nav links
+const navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach((link) =>
+    link.addEventListener("click", () => {
+        ul.classList.remove("show");
+    })
+);
